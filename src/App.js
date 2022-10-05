91ced76ce47e7ffbe55c1e9d490650687fc6c22b
natdanai <natdanai.thaiyala@gmail.com>
2022-10-05 16:43:57 +0700

    Update
    การประกาศตัวแปร

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default () => {
-  return <div>codemobile</div>;
+  const var1 = 0;
+  const var2 = "Hello";
+  const var3 = true;
+
+  return (
+    <div>
+      {/*การเรียกใช้ตัวเเปร*/}
+      <h1>codemobile {var1}</h1>
+      <h1>codemobile {var2}</h1>
+      {/*การเรียกใช้ตัวเเปรเเบบมีเงื่อนไข*/}
+      <h1>codemobile {var3 && "Yes"}</h1>
+      {var3 ? <h1></h1>}
+    </div>
+  );
 };
