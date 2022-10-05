ffc7d0558b38d68d4c5c4f1dfb2a4bdd8fc0d44f
iiimomoniii <natdanai_thaiyala@live.com>
2022-10-05 17:24:37 +0700

    Update
    ใช้คำสั่ง onClick เพื่อเพิ่มค่าให้กับ var1 เเต่ใน react มันต้องมีการ update ค่าโดยใช้ function useState รับค่าเข้าไปเพื่อทำการเปลี่ยนเเปลงค่าที่ต้องการ

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
-  const var1 = 0;
+  let var1 = 0;
   const var2 = "Hello";
   const var3 = true;
-
+  //count คือคัวเเปร ส่วน setCount คือ function ในการเปลี่ยนเเปลงค่า count
+  //useState(0) => คือการประกาศ let count = 0
+  const [count, setCount] = useState(0);
   return (
     <div>
       {/*การเรียกใช้ตัวเเปร*/}
@@ -13,6 +15,18 @@ export default () => {
       {/*การเรียกใช้ตัวเเปรเเบบมีเงื่อนไข*/}
       <h1>codemobile {var3 && "Yes"}</h1>
       {var3 ? <h1>codemobile</h1> : <h1>NO</h1>}
+      {/*เแสดงค่า count จากเริ่มต้นเเละที่เกิดการเปลี่ยนเเปลงจาก function setCount*/}
+      <h1>Count : {count}</h1>
+      <button
+        onClick={() => {
+          var1 = var1 + 1;
+          //function setCount รับค่า count เข้าไป +1
+          setCount(count + 1);
+          console.log(var1);
+        }}
+      >
+        ADD
+      </button>
     </div>
   );
 };
